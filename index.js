0997123dc2bc0f25e39a85e505a7bb94e46950de
Nobushige Tsuchiya <n.tsuchiya@pathcreate.com>
2019-04-23 13:31:08 +0900

    Now 100 events

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function listEvents(auth) {
     if (err) return console.log('The API returned an error: ' + err);
     const events = res.data.items;
     if (events.length) {
-      console.log('Upcoming 10 events:');
+      console.log('# Upcoming 100 events:');
       events.map((event, i) => {
         const start = event.start.dateTime || event.start.date;
         console.log(`${start} - ${event.summary}`);
